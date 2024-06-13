04dee47ed8f511ab42136abe4dad6f76ac0103e8
inns21 <inns2110@gmail.com>
2024-06-13 22:58:11 +0900

    feat : Header
    상단바 제작

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from "../Components/Header/Header.tsx"
 
 function HomePage() {
   return (
     <div>
+      <Header></Header>
+      <h1 className="text-2xl font-bold">Home Page</h1>
       <h1 className="text-2xl font-bold">Home Page</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'logo-color' : '#F6CF83',
+        'my-color': '#444A6B',
+        'dark-color' : '#2D3146',
+      },
+    },
   },
   plugins: [],
 }
